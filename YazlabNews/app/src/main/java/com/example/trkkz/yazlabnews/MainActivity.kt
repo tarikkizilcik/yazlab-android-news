73e07f0ce3a9cc01773f518c047e0972ead16d77
tarikkizilcik <trk.kzlck@hotmail.com>
2019-04-10 03:32:50 +0300

    Create Android Studio project
    3.1.3

---
+++ YazlabNews/app/src/main/java/com/example/trkkz/yazlabnews/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trkkz.yazlabnews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
