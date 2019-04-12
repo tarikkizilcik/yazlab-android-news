e716c8f5cd7cc1e4ae4a98c616ddb3af2435f243
emirhanaydin <emirhanaydin95@gmail.com>
2019-04-13 00:35:57 +0300

    Code cleanup

--- YazlabNews/app/src/main/java/com/example/trkkz/yazlabnews/NewsActivity.kt
+++ YazlabNews/app/src/main/java/com/example/trkkz/yazlabnews/NewsActivity.kt
@@ -3,59 +3,64 @@ package com.example.trkkz.yazlabnews
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.widget.Toast
+import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.trkkz.yazlabnews.adapters.NewsAdapter
 import com.example.trkkz.yazlabnews.data.News
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_news.*
 import org.json.JSONArray
 import org.json.JSONObject
 
+private const val TAG = "NewsActivity"
+
 class NewsActivity : AppCompatActivity() {
     private val newsList = mutableListOf<News>()
+    private val newsAdapter = NewsAdapter(newsList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
-//        val newsAdapter = NewsAdapter(newsList)
-//        recyclerViewNews.apply {
-//            setHasFixedSize(true)
-//            layoutManager = LinearLayoutManager(this@NewsActivity)
-//            adapter = newsAdapter
-//        }
+        recyclerViewNews.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@NewsActivity)
+            adapter = newsAdapter
+        }
 
-        // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val url = "http:/10.0.3.2:3000/api/news"
+        val url = "${getString(R.string.url)}/api/news"
 
-// Request a string response from the provided URL.
         val stringRequest = StringRequest(Request.Method.GET, url,
-                Response.Listener<String> { response ->
-                    // Display the first 500 characters of the response string.
-                    val jsonArray = JSONArray(response)
-                    for (i: Int in 0 until jsonArray.length()) {
-                        val jsonObject = jsonArray[i] as JSONObject
-                        val author = jsonObject.getString("author")
-                        val title = jsonObject.getString("title")
-                        val body = jsonObject.getString("body")
+                responseListenerGetNews,
+                errorListenerGetNews)
 
-                        newsList.add(News(author, title, body))
-                    }
-
-                    val newsAdapter = NewsAdapter(newsList)
-                    recyclerViewNews.apply {
-                        setHasFixedSize(true)
-                        layoutManager = LinearLayoutManager(this@NewsActivity)
-                        adapter = newsAdapter
-                    }
-                },
-                Response.ErrorListener { Toast.makeText(this, "An error occurred", Toast.LENGTH_LONG).show() })
-
-// Add the request to the RequestQueue.
         queue.add(stringRequest)
     }
+
+    private val responseListenerGetNews = Response.Listener<String> {
+        // Add all messages to the list in the response
+        val gson = Gson()
+
+        val jsonArray = JSONArray(it)
+        for (jsonObject in jsonArray) {
+            if (jsonObject !is JSONObject) continue
+
+            val news: News = gson.fromJson(jsonObject.toString(), News::class.java)
+            newsList.add(news)
+        }
+
+        this@NewsActivity.runOnUiThread {
+            newsAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private val errorListenerGetNews = Response.ErrorListener {
+        it.printStackTrace()
+        Log.e(TAG, it.localizedMessage)
+    }
 }
+
